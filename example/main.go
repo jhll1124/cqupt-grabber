e017379adf9172750e16b6a163969de45d3aad28
镜花落璃 <109937757+jhll1124@users.noreply.github.com>
2024-11-13 09:48:34 +0800

    更新 main.go

--- example/main.go
+++ example/main.go
@@ -1,19 +1,16 @@
 package main
 
-import (
-	"cqupt"
-)
+import "cqupt"
 
 func main() {
-	tool := cqupt.New()
+  tool := cqupt.New()
 
-	cookie := "这里是一个cookie"
+  cookie := "这里是一个cookie"
 
-	// 支持同时抢多门课程
-	loads := []string{
-		"这里是第一节课",
-		"这里是第二节课",
-	}
+  //支持同时抢多门课程
+  loads := []string{
+    "这里是第一节课",
+    "这里是第二节课"}
 
-	tool.Grabber.LoopRob(cookie, loads)
+  tool.Grabber.LoopRob(cookie, loads)
 }
